4986de84ee19524bb28eec5b7cd9556503b38b02
Paweł Psztyć <jarrodek@gmail.com>
2016-04-06 14:52:57 +0100

    Update: Changed hows the element recognize the import location

--- import-location.js
+++ import-location.js
@@ -3,6 +3,10 @@
   if (this.currentImport && this.currentImport.URL) {
     var url = this.currentImport.URL;
     var path = url.substr(url.indexOf('/', url.indexOf('/') + 2)).replace('socket-fetch.html','');
+    var lastDot = path.lastIndexOf('.');
+    if (lastDot !== -1 && path.substr(lastDot + 1) === 'html') {
+      path = path.substr(path.lastIndexOf('/'));
+    }
     path += '%s';
     SocketFetchOptions.importUrl = path;
   }
