cc92a54417f8046c6e17b0f3f9346a745c028523
jarrodek <jarrodek@gmail.com>
2016-04-11 16:28:14 +0100

    Fix: fixed invalid variable name in the decompression worker

--- decompress-worker.js
+++ decompress-worker.js
@@ -1,13 +1,15 @@
+/* global Zlib, importScripts, self */
 importScripts('../zlib/bin/zlib_and_gzip.min.js');
 
 self.addEventListener('message', function(e) {
   var buffer = e.data.buffer;
   var compression = e.data.compression;
+  var inflate;
   if (compression.indexOf('gzip') !== -1) {
-    var inflate = new Zlib.Gunzip(buffer);
+    inflate = new Zlib.Gunzip(buffer);
     buffer = inflate.decompress();
-  } else if (ce.indexOf('deflate') !== -1) {
-    var inflate = new Zlib.Inflate(buffer);
+  } else if (compression.indexOf('deflate') !== -1) {
+    inflate = new Zlib.Inflate(buffer);
     buffer = inflate.decompress();
   }
   self.postMessage(buffer);
